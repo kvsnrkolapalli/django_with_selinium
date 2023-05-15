cfab6faee4c628ef3ee80b8f9310f70abaa82e70
Nageswara Rao Kolapalli <110399609+kvsnrkolapalli@users.noreply.github.com>
2023-05-15 19:19:11 +0530

    Update settings.py

--- WebApp/settings.py
+++ WebApp/settings.py
@@ -116,3 +116,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, "sattic/")
+ALLOWED_HOSTS = ['0.0.0.0','']
